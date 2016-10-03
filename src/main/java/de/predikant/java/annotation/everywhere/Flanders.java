433130c500888deb3a3eb32d367af12c0bbd32bb
Christian Predikant <christian.predikant@gmail.com>
2016-10-03 20:30:02 +0200

    Annotations everywhere

---
+++ src/main/java/de/predikant/java/annotation/everywhere/Flanders.java
@@ -0,0 +1,9 @@
+package de.predikant.java.annotation.everywhere;
+
+public class Flanders <@PrayEverywhere T> extends @PrayEverywhere Object {
+
+	public @PrayEverywhere Object pray(@PrayEverywhere Object suffrage) throws @PrayEverywhere Exception {
+		return null;
+	}
+	
+}
---
+++ src/main/java/de/predikant/java/annotation/everywhere/PrayFlanders.java
@@ -0,0 +1,13 @@
+package de.predikant.java.annotation.everywhere;
+
+public class PrayFlanders {
+	
+	private PrayFlanders(){
+		// Only static methods.
+	}
+
+	public static void doIt(){
+		@PrayEverywhere Flanders< @PrayEverywhere String> f = new @PrayEverywhere Flanders< @PrayEverywhere String>(); 
+	}
+	
+}
